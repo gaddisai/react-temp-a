39974011dc7a4696af68f70582bf95e5915b8225
gaddisai <saikrishna.gaddii@gmail.com>
2025-04-03 13:50:59 +0530

    changes done

---
+++ src/components/App17.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App17() {
+  return (
+    <>
+    <div className='bottom_content17'>
+        <h2>This is App17</h2>
+        <div>
+            {/* <FaHome size={15}/>
+            <label>Home</label> */}
+        </div>
+    </div>
+    </>
+  )
+}
+
+export {App17}
---
+++ src/components/App18.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App18() {
+  return (
+    <>
+        <div className='bottom_content18'>
+            <h2>This is App18</h2>
+            <div>
+                {/* <FaHome size={15}/>
+                <label>Home</label> */}
+            </div>
+        </div>
+    </>
+  )
+}
+
+export {App18}
